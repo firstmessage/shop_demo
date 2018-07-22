163d01e8f1452b76512581e1112074362ed1a161
Ghermas Denis <firstmessage@gmail.com>
2018-07-22 19:33:20 +0300

    add django stage-2

---
+++ shop_app/admin.py
@@ -0,0 +1,9 @@
+# импортируем из джанго методы добавления в админку  
+from django.contrib import admin 
+# импортируем нашу модель
+from .models import Product
+from .models import Category
+
+# говорим админке зарегистрировать нашу модель
+admin.site.register(Product)
+admin.site.register(Category)
